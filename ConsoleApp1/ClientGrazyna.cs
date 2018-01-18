5f793df591d9114dd6d758e4cee289bbc79401c5
edytamaria <bandyta.edyta@gmail.com>
2018-01-18 18:06:46 +0100

    POP3 Client, c#
    Simple POP3 Client, check if any new messages in your mailbox

---
+++ ConsoleApp1/ClientGrazyna.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net.Security;
+using System.Net.Sockets;
+
+namespace ConsoleApp1
+{
+    public class ClientGrazyna
+    {
+        private TcpClient tcpClient;
+        private SslStream sslStream;
+        private StreamReader streamReader;
+        private StreamWriter streamWriter;
+
+        public ClientGrazyna(string HOST, int PORT)
+        {
+            this.tcpClient = new TcpClient();
+            this.tcpClient.Connect(HOST, PORT);
+
+            this.sslStream = new SslStream(this.tcpClient.GetStream());
+            this.sslStream.AuthenticateAsClient(HOST);
+
+            this.streamReader = new StreamReader(this.sslStream);
+            this.streamWriter = new StreamWriter(this.sslStream);
+        }
+
+        public void Write(string message)
+        {
+            this.streamWriter.WriteLine(message);
+            this.streamWriter.Flush();
+        }
+
+        public void WriteAndPrintIntoConsole(string message)
+        {
+            this.Write(message);
+            this.PrintIntoConsole();
+        }
+
+        public void PrintIntoConsole()
+        {
+            Console.WriteLine(this.streamReader.ReadLine());
+        }
+
+        public string ReadMessage()
+        {
+            string message = "", temp = "";
+            while ((temp = this.streamReader.ReadLine()) != null)
+            {
+                if (temp == "." || temp.IndexOf("-ERR") != -1)
+                {
+                    break;
+                }
+
+                message += temp + "\n";
+            }
+            return message;
+        }
+
+        public void Close()
+        {
+            this.tcpClient.Close();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Configuration;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static string GetConfig(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        public static List<string> ReadMailbox(string HOST, int PORT, string USER, string PASS, string strTemp, List<string> UIDL)
+        {
+            var client = new ClientGrazyna(HOST, PORT);
+
+            client.WriteAndPrintIntoConsole("USER " + USER);
+            client.WriteAndPrintIntoConsole("PASS " + PASS);
+
+            client.Write("LIST");
+            var responseMultiline = client.ReadMessage().Split('\n');
+
+            foreach (var line in responseMultiline)
+            {
+                string[] strarr = line.Split(' ');
+                Console.WriteLine(strarr[0]);
+                if (strarr.Length == 2)
+                {
+                    UIDL.Add(strarr[1]);
+                    Console.WriteLine(strarr[1]);
+                }
+            }
+            return UIDL;
+        }
+
+        public static List<string> CheckMailbox(string HOST, int PORT, string USER, string PASS, string strTemp, List<string> UIDL, int counter)
+        {
+            var client = new ClientGrazyna(HOST, PORT);
+
+
+            client.WriteAndPrintIntoConsole("USER " + USER);
+            client.WriteAndPrintIntoConsole("PASS " + PASS);
+
+            client.Write("LIST");
+            var responseMultiline = client.ReadMessage().Split('\n');
+
+            foreach (var line in responseMultiline)
+            {
+                UIDL.Insert(0, counter.ToString());
+                string[] strarr = line.Split(' ');
+                if (strarr.Length == 2)
+                {
+                    UIDL.Remove(counter.ToString());
+                    if (!UIDL.Contains(strarr[1]))
+                    {
+                        UIDL.Add(strarr[1]);
+                        Console.WriteLine("Check your mailbox.");
+                        counter++;
+                        UIDL.Insert(0, counter.ToString());
+                    }
+
+                }
+            }
+            return UIDL;
+        }
+
+        private static void RefreshMailbox(string HOST, int PORT, string USER, string PASS, string strTemp, List<string> UIDL, int count)
+        {
+            using (TcpClient tc = new TcpClient())
+            {
+                tc.Connect(HOST, PORT);
+                using (SslStream sl = new SslStream(tc.GetStream()))
+                {
+                    sl.AuthenticateAsClient(HOST);
+
+                    CheckMailbox(HOST, PORT, USER, PASS, strTemp, UIDL, count);
+                }
+                tc.Close();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            string HOST = GetConfig("HOST");
+            string USER = GetConfig("USER");
+            int PORT = Int32.Parse(GetConfig("PORT"));
+            string PASS = GetConfig("PASS");
+            int TIME = Int32.Parse(GetConfig("TIME"));
+            int counter = 0;
+
+            string str = string.Empty;
+            string strTemp = string.Empty;
+            List<string> UIDL = new List<string>();
+
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromSeconds(TIME);
+
+            ReadMailbox(HOST, PORT, USER, PASS, strTemp, UIDL);
+
+            using (var timer = new System.Threading.Timer((e) =>
+             {
+                 RefreshMailbox(HOST, PORT, USER, PASS, strTemp, UIDL, counter);
+             }, null, startTimeSpan, periodTimeSpan)) { Console.ReadKey(); }
+
+            //timer.Dispose();
+
+            Console.WriteLine(String.Format("\nIn this session you recived {0} emails.", UIDL[0]));
+            Console.ReadKey();
+
+        }
+
+
+    }
+
+}
+
+
